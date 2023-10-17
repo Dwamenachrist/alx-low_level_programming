dc449688b0df5202460b54b8cbc0affbaf058b0d
Dwamenachrist <Agyapongfrancis520@gmail.com>
2023-10-17 05:11:03 -0700

    done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+    int i = 0;
 
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+        if (i >= 98)
+            break;
+    }
+    dest[i] = '\0';
 
-	return (dest);
+    return dest;
 }
