8b9785e67a0e32c5aad78aa83d8d1e44b488fa1a
Dwamenachrist <Agyapongfrancis520@gmail.com>
2023-11-10 15:20:42 -0800

    ALX

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_elem - Prints an integer element.
+ * @elem: The integer element to print.
+ */
 void print_elem(int elem)
 {
 	printf("%d\n", elem);
 }
 
+/**
+ * print_elem_hex - Prints an integer element in hexadecimal format.
+ * @elem: The integer element to print.
+ */
 void print_elem_hex(int elem)
 {
 	printf("0x%x\n", elem);
 }
 
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
 int main(void)
 {
 	int array[5] = {0, 98, 402, 1024, 4096};
